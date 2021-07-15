c01a000729e8e6f928a321f3030ec999961ff8a1
SAKHI Abdelkader <abdelkader.sakhi.pro@gmail.com>
2021-07-15 16:29:20 +0200

    Classe React

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import TodoApp from './TodoApp';
 
 function App() {
   return (
     <div >
       <h1>Hello React</h1>
+      <TodoApp/>
     </div>
   );
 }
---
+++ todo/src/TodoApp.js
@@ -0,0 +1,10 @@
+
+import React,{Component} from 'react';
+
+export default class TodoApp extends Component{
+    render(){
+        return(<div>
+            Todo App
+        </div>);
+    }
+}
