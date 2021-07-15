1bf26ddd08c719bd6c7b8aeaafd5be7a58cc425a
SAKHI Abdelkader <abdelkader.sakhi.pro@gmail.com>
2021-07-15 16:45:54 +0200

    Librairie Bootstrap

--- todo/src/TodoApp.js
+++ todo/src/TodoApp.js
@@ -3,8 +3,24 @@ import React,{Component} from 'react';
 
 export default class TodoApp extends Component{
     render(){
-        return(<div>
-            Todo App
+        return(
+        <div className="container">
+            <br/>
+            <div className="row">
+                <div className="col-md-6">
+                    List Undone
+                    <ul>
+                        <li>Item #1</li>
+                        <li>Item #2</li>
+                    </ul>
+                </div>
+                <div className="col-md-6">
+                    List Done
+                    <li>Item #1</li>
+                    <li>Item #2</li>
+                    <li>Item #3</li>
+                </div>
+            </div>
         </div>);
     }
 }
