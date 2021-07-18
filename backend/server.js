ab0cc3516d2c967c00dd60b307ab9860747b8c0a
SAKHI Abdelkader <abdelkader.sakhi.pro@gmail.com>
2021-07-18 14:20:11 +0200

    MongoDB : Connecter la base de donnée

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,9 @@ const router = express.Router();
 app.listen(4000, () => {
     console.log('server is up and running on port 4000');
 });
+
+const db ="mongodb+srv://abdelkader:Mokht%40r2021@items-todos.yjq2j.mongodb.net/test";
+
+mongoose.connect(db, {useNewUrlParser: true })
+.then(() => console.log("successfully connected to db"))
+.catch(err => console.error(err));
